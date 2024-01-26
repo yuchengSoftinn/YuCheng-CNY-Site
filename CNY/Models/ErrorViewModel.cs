c519c223a1e4c399636a71ae5bfb8e32a23e0294
YuZhe0690 <chengycchong@gmail.com>
2024-01-26 13:45:52 +0800

    Add project files.

---
+++ CNY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CNY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
